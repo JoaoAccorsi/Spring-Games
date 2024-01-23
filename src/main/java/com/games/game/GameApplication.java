71f9b11152e8e0a0e93ffd40c7fd984bef94f6b1
joao <joaovaccorci@gmail.com>
2024-01-23 14:55:22 -0300

    Fix: Specify the port in the Dockerfile

--- src/main/java/com/games/game/GameApplication.java
+++ src/main/java/com/games/game/GameApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GameApplication {
 
+	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(GameApplication.class, args);
 	}
